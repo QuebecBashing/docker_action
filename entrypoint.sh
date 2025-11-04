70d01e13ab24224569c17bb704022b1be1a139cf
Sébastien Graveline <graveline.seb@gmail.com>
2025-11-04 16:14:22 -0500

    Add curl installation and example usage

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,8 @@ echo "::notice file=entrypoint.sh,line=7::$GREETING"
 # Write outputs to the $GITHUB_OUTPUT file
 echo "time=$(date)" >>"$GITHUB_OUTPUT"
 
+apk add curl
+
 curl https://example.com
 
 exit 0
